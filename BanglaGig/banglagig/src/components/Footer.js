be2d0baafc548bc57e42eb72a8b95a313d1d0503
ArnobsShoeb <arnobshoeb@gmail.com>
2024-06-18 13:38:06 +0600

    added login functionality including connect with database

--- BanglaGig/banglagig/src/components/Footer.js
+++ BanglaGig/banglagig/src/components/Footer.js
@@ -4,117 +4,117 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <div>
-      <footer class="text-center text-lg-start bg-body-tertiary text-muted">
-        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-          <div class="me-5 d-none d-lg-block">
+      <footer className="text-center text-lg-start bg-body-tertiary text-muted">
+        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+          <div className="me-5 d-none d-lg-block">
             <span>Get connected with us on social networks:</span>
           </div>
 
           <div>
-            <Link to="/" class="me-4 text-reset">
-              <i class="fab fa-facebook-f"></i>
+            <Link to="/" className="me-4 text-reset">
+              <i className="fab fa-facebook-f"></i>
             </Link>
-            <Link to="/" class="me-4 text-reset">
-              <i class="fab fa-twitter"></i>
+            <Link to="/" className="me-4 text-reset">
+              <i className="fab fa-twitter"></i>
             </Link>
-            <Link to="/" class="me-4 text-reset">
-              <i class="fab fa-google"></i>
+            <Link to="/" className="me-4 text-reset">
+              <i className="fab fa-google"></i>
             </Link>
-            <Link to="/" class="me-4 text-reset">
-              <i class="fab fa-instagram"></i>
+            <Link to="/" className="me-4 text-reset">
+              <i className="fab fa-instagram"></i>
             </Link>
-            <Link to="/" class="me-4 text-reset">
-              <i class="fab fa-linkedin"></i>
+            <Link to="/" className="me-4 text-reset">
+              <i className="fab fa-linkedin"></i>
             </Link>
-            <Link to="/" class="me-4 text-reset">
-              <i class="fab fa-github"></i>
+            <Link to="/" className="me-4 text-reset">
+              <i className="fab fa-github"></i>
             </Link>
           </div>
         </section>
 
-        <section class="">
-          <div class="container text-center text-md-start mt-5">
-            <div class="row mt-3">
-              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <h6 class="fw-bold mb-4">
-                  <i class="fas fa-gem me-3"></i>BanglaGig
+        <section className="">
+          <div className="container text-center text-md-start mt-5">
+            <div className="row mt-3">
+              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <h6 className="fw-bold mb-4">
+                  <i className="fas fa-gem me-3"></i>BanglaGig
                 </h6>
                 <p>
                 Connecting talented freelancers with businesses worldwide. Delivering quality work, one project at a time. Join our community and elevate your career or find the perfect talent for your project
                 </p>
               </div>
 
-              <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 class="text-uppercase fw-bold mb-4">Products</h6>
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Products</h6>
                 <p>
-                  <Link href="#!" class="text-reset">
+                  <Link href="#!" className="text-reset">
                     Angular
                   </Link>
                 </p>
                 <p>
-                  <Link href="#!" class="text-reset">
+                  <Link href="#!" className="text-reset">
                     React
                   </Link>
                 </p>
                 <p>
-                  <Link href="#!" class="text-reset">
+                  <Link href="#!" className="text-reset">
                     Vue
                   </Link>
                 </p>
                 <p>
-                  <Link href="#!" class="text-reset">
+                  <Link href="#!" className="text-reset">
                     Laravel
                   </Link>
                 </p>
               </div>
 
-              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
                 <p>
-                  <Link href="#!" class="text-reset">
+                  <Link href="#!" className="text-reset">
                     Pricing
                   </Link>
                 </p>
                 <p>
-                  <Link href="#!" class="text-reset">
+                  <Link href="#!" className="text-reset">
                     Settings
                   </Link>
                 </p>
                 <p>
-                  <Link href="#!" class="text-reset">
+                  <Link href="#!" className="text-reset">
                     Orders
                   </Link>
                 </p>
                 <p>
-                  <Link href="#!" class="text-reset">
+                  <Link href="#!" className="text-reset">
                     Help
                   </Link>
                 </p>
               </div>
 
-              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
                 <p>
-                  <i class="fas fa-home me-3"></i> Merul Badda,Dhaka 1200, BD
+                  <i className="fas fa-home me-3"></i> Merul Badda,Dhaka 1200, BD
                 </p>
                 <p>
-                  <i class="fas fa-envelope me-3"></i>
+                  <i className="fas fa-envelope me-3"></i>
                   arnobshoeb@gmail.com
                 </p>
                 <p>
-                  <i class="fas fa-phone me-3"></i> +880 1737 208993
+                  <i className="fas fa-phone me-3"></i> +880 1737 208993
                 </p>
                 <p>
-                  <i class="fas fa-print me-3"></i> +880 1973 208993
+                  <i className="fas fa-print me-3"></i> +880 1973 208993
                 </p>
               </div>
             </div>
           </div>
         </section>
 
-        <div class="text-center p-4">
+        <div className="text-center p-4">
           © 2021 Copyright:
-          <Link class="text-reset fw-bold" href="https://mdbootstrap.com/">
+          <Link className="text-reset fw-bold" href="https://mdbootstrap.com/">
             MDBootstrap.com
           </Link>
         </div>
--- BanglaGig/banglagig/src/components/Navbar.js
+++ BanglaGig/banglagig/src/components/Navbar.js
@@ -5,25 +5,25 @@ export default function Navbar() {
   return (
    
     <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <Link class="navbar-brand fs-1 fst-italic" to="/">BanglaGig</Link>
-    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <Link className="navbar-brand fs-1 fst-italic" to="/">BanglaGig</Link>
+    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/">Features</Link>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Features</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/">Pricing</Link>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Pricing</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link " to="/login" >Login</Link>
+        <li className="nav-item">
+          <Link className="nav-link " to="/login" >Login</Link>
         </li>
       </ul>
     </div>
